852143da7dfcb0cb691563d13222d7235ba7b625
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-09-28 13:02:48 -0400

    added idle animation + cleaned up collisions

--- matthews-journey/Player/Scripts/player.gd
+++ matthews-journey/Player/Scripts/player.gd
@@ -5,6 +5,7 @@ class_name Player extends CharacterBody2D
 
 const SPEED = 175.0
 var direction : Vector2 = Vector2.DOWN
+var cardinal_direction : Vector2 = Vector2.DOWN
 
 func _process(delta: float) -> void:
 	# Get the direction the player is pressing
@@ -18,19 +19,28 @@ func _process(delta: float) -> void:
 	velocity.y = direction.y * SPEED
 	
 	if direction == Vector2.ZERO:
-		animation_player.pause()
+		if cardinal_direction == Vector2.DOWN:
+			animation_player.play("idle_down")
+		elif cardinal_direction == Vector2.UP:
+			animation_player.play("idle_up")
+		else:
+			animation_player.play("idle_side")
 	elif direction == Vector2.DOWN:
 		animation_player.play("walk_down")
+		cardinal_direction = Vector2.DOWN
 	elif direction == Vector2.UP:
 		animation_player.play("walk_up")
+		cardinal_direction = Vector2.UP
 	elif direction == Vector2.RIGHT:
 		# flip sprite right
 		sprite.scale.x = 1
 		animation_player.play("walk_side")
+		cardinal_direction = Vector2.RIGHT
 	elif direction == Vector2.LEFT:
 		# flip sprite left
 		sprite.scale.x = -1
 		animation_player.play("walk_side")
+		cardinal_direction = Vector2.LEFT
 		
 	
 	
