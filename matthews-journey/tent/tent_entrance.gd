156bf7d0a9f6dd5ffafd55defa61ea34cd2898be
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-10-08 12:08:02 -0400

    updates

--- matthews-journey/tent/tent_entrance.gd
+++ matthews-journey/tent/tent_entrance.gd
@@ -7,8 +7,8 @@ func _on_Area2D_body_entered(body: PhysicsBody2D):
 
 func _on_Area2D_body_exited(body):
 	entered = false
-	
+
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed( "ui_accept"):
-			get_tree().change_scene("res://InteriorTent.tscn")
+			get_tree().change_scene("res://game.tscn")
