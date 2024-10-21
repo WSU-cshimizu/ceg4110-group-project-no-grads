0c23a1f2d51b95d9c8d08d97615fbc93c292577e
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-10-21 18:22:44 -0400

    added player state machine with idle, walk and attack

--- matthews-journey/Player/Scripts/player.gd
+++ matthews-journey/Player/Scripts/player.gd
@@ -1,15 +1,28 @@
 class_name Player extends CharacterBody2D
 
+# Health Variables
 signal healthChanged #VR
 @export var maxHealth = 5 #VR
 @onready var currentHealth: int = maxHealth #VR
 
+# Sprite variables
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
+# Direction Variables
 const SPEED = 175.0
-var direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
 var cardinal_direction : Vector2 = Vector2.DOWN
+const DIRECTIONS = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
+
+# State Variables
+var current_state : String = "idle"
+const ATTACK_DELAY: float = 0.35   # delay between player attacks
+var attack_cooldown: float = 0.0  # current time delay until player can attack
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
 
 func _process(delta: float) -> void:
 	# Get the direction the player is pressing
@@ -18,34 +31,61 @@ func _process(delta: float) -> void:
 		Input.get_axis("up", "down")
 	# normalize the vector to magnitude 1. Prevents faster movement in diagonal.
 	).normalized() 
+	
+	update_state(delta)
+	update_direction()
+	update_animation()
+
+		
 	if($"../PlayerChatIcon".is_chatting):
 		direction = Vector2.ZERO
-	velocity.x = direction.x * SPEED
-	velocity.y = direction.y * SPEED
+		
+	velocity = direction * SPEED
 	
+# Source for direction and animation updating: Michael Games AARPG tutorial series on youtube	
+	
+# Return's true if the cardinal direction has changed based on player direction, false if it has not.
+# The function will also update the cardinal direction if direction has changed enough.
+func update_direction() -> bool:
+	# if no player input then return false
 	if direction == Vector2.ZERO:
-		if cardinal_direction == Vector2.DOWN:
-			animation_player.play("idle_down")
-		elif cardinal_direction == Vector2.UP:
-			animation_player.play("idle_up")
+		return false
+	
+	# Calculate current cardinal direction
+	var direction_id : int = int(round( 
+		# bias the direction towards the current cardinal direction to prevent weird player motion (example: moonwalking)
+		(direction + cardinal_direction * 0.1  
+		# Divide the angle of direction by 2*PI * 4. This converts direction to an integer 0-3 corresponding to an index in DIRECTIONS
+		).angle() / TAU * DIRECTIONS.size()))
+	# assign the new direction based on calculation above
+	var new_dir : Vector2 = DIRECTIONS[direction_id]
+	
+	# If player still facing the same direction as before return false
+	if new_dir == cardinal_direction:
+		return false
+	
+	# update the direction, flip sprite if needed, and return true (direction changed)
+	else:
+		cardinal_direction = new_dir
+		# flip the sprite if facing left
+		if cardinal_direction == Vector2.LEFT:
+			sprite.scale.x = -1
 		else:
-			animation_player.play("idle_side")
-	elif direction == Vector2.DOWN:
-		animation_player.play("walk_down")
-		cardinal_direction = Vector2.DOWN
-	elif direction == Vector2.UP:
-		animation_player.play("walk_up")
-		cardinal_direction = Vector2.UP
-	elif direction == Vector2.RIGHT:
-		# flip sprite right
-		sprite.scale.x = 1
-		animation_player.play("walk_side")
-		cardinal_direction = Vector2.RIGHT
-	elif direction == Vector2.LEFT:
-		# flip sprite left
-		sprite.scale.x = -1
-		animation_player.play("walk_side")
-		cardinal_direction = Vector2.LEFT
+			sprite.scale.x = 1
+		return true
+
+# update the character animation based on state and current cardinal direction	
+func update_animation() -> void:
+	animation_player.play(current_state + "_" + animation_direction())
+
+# Return a string representing the players current facing cardinal direction
+func animation_direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
 		
 func handleCollision(): #VR
 	for i in get_slide_collision_count():
@@ -60,6 +100,23 @@ func _on_hurt_box_area_entered(area): #VR
 		if currentHealth < 0:
 			currentHealth = maxHealth
 		healthChanged.emit(currentHealth)
+		
+func update_state(delta: float) -> void:
+	var state: String = current_state
+	
+	if attack_cooldown > 0:
+		attack_cooldown -= delta
+		
+	if Input.is_action_pressed("attack"):
+		if attack_cooldown <= 0:
+			attack_cooldown = ATTACK_DELAY
+			state = "attack"
+	elif direction == Vector2.ZERO:
+		state = "idle" # idle
+	elif direction != Vector2.ZERO:
+		state = "walk"
+
+	current_state = state
 
 
 func _physics_process(delta: float) -> void:
