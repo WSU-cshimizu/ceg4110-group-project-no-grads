72173ff537779e9a0f00722996bf18c235dd78d5
caraz22 <cszoke22@gmail.com>
2024-10-22 11:11:58 -0400

    menu on startup

--- matthews-journey/main_menu.gd
+++ matthews-journey/main_menu.gd
@@ -13,12 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_begin_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
-asdf
-w
-asassfunc _on_controls_pressed() -> void:
-swdas	wvar controls = load("res://controls_menu.tscn").instance()
-ssa	gdaetss_tree().current_scene.add_child(controls)
-wwwsd
+	
+func _on_controls_pressed() -> void:
+	var controls = load("res://controls_menu.tscn").instance()
+	get_tree().current_scene.add_child(controls)
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
