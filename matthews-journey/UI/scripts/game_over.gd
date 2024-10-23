abf44d7cef2d89bcdce09b9560dc27c6d99b34cf
Virginia Richards <richards.76@wright.edu>
2024-10-23 19:37:58 -0400

    Added a deathscreen, campfire, & did cleanup

--- matthews-journey/UI/scripts/game_over.gd
+++ matthews-journey/UI/scripts/game_over.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
-		get_tree().change_scene_to_file("res://main_menu.tscn")
+		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
--- matthews-journey/UI/scripts/main_menu.gd
+++ matthews-journey/UI/scripts/main_menu.gd
@@ -15,7 +15,7 @@ func _on_begin_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 	
 func _on_controls_pressed() -> void:
-	var controls = load("res://controls_menu.tscn").instance()
+	var controls = load("res://UI/controls_menu.tscn").instance()
 	get_tree().current_scene.add_child(controls)
 
 func _on_quit_game_pressed() -> void:
