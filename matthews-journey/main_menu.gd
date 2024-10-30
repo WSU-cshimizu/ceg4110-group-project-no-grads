bbff722f8308f0336a54b77e0ba47b74c344cdfb
RomanRhino <gatrios@woh.rr.com>
2024-10-30 15:51:46 -0700

    Added control screen

--- matthews-journey/main_menu.gd
+++ matthews-journey/main_menu.gd
@@ -13,9 +13,12 @@ func _process(delta: float) -> void:
 
 func _on_begin_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
-	
+
+
 func _on_controls_pressed() -> void:
-	get_tree().change_scene_to_file("res://UI/controls_menu.tscn")
+	var controls = load("res://controls_menu.tscn").instance()
+	get_tree().current_scene.add_child(controls)
+
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
