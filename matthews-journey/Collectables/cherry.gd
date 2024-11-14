66f68e9895a839d0468604aa40bf000f1b8017c3
Virginia Richards <richards.76@wright.edu>
2024-11-13 19:27:13 -0500

    Added Cherry that refills health

---
+++ matthews-journey/Collectables/cherry.gd
@@ -0,0 +1,17 @@
+class_name Cherry
+extends Collectable #inheirits from collectable.gd
+
+@onready var animations = $AnimationPlayer
+@onready var heartsContainer = $MarginContainer/heartsContainer 
+
+func collect():
+	var player = get_tree().get_first_node_in_group("player")
+	
+	print("Before healing - Current Health:", player.currentHealth)
+	player.currentHealth = player.maxHealth
+	print("After healing - Current Health:", player.currentHealth, " Max Health:", player.maxHealth)
+	
+	player.healthChanged.emit(player.currentHealth)
+	animations.play("spin")
+	await animations.animation_finished
+	super.collect()
