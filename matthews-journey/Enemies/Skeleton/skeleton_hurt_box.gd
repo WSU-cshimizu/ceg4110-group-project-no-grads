0e3fc4a7e18dc1d48c8594a7905e7cd9c0683732
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-11-15 11:23:23 -0500

    enemies drop coins on defeat

--- matthews-journey/Enemies/Skeleton/skeleton_hurt_box.gd
+++ matthews-journey/Enemies/Skeleton/skeleton_hurt_box.gd
@@ -2,6 +2,9 @@ class_name SkeletonHurtBox extends HurtBox
 
 @onready var skeleton: Skeleton = $".."
 var player: Player
+@onready var game: Node2D = $"."
+const COIN_03 = preload("res://Collectables/Currency/coin03.tscn")
+var drop_count = 4
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -11,6 +14,11 @@ func take_attack(attack: Attack):
 	skeleton.health -= attack.damage
 	if skeleton.health <= 0:
 		player.collect_xp(skeleton.xp)
+		for i in range(drop_count):
+			var coin: Coin03 = COIN_03.instantiate()
+			skeleton.get_parent().call_deferred("add_child", coin)
+			var spawn_offset = Vector2(randi_range(1, 15), randi_range(1, 15))
+			coin.global_position = skeleton.global_position + spawn_offset
 		skeleton.queue_free()
 
 func knockback(direction: Vector2, force: int) -> void:
--- matthews-journey/Enemies/Slime/slime_hurt_box.gd
+++ matthews-journey/Enemies/Slime/slime_hurt_box.gd
@@ -2,6 +2,8 @@ class_name SlimeHurtBox extends HurtBox
 
 @onready var slime: Slime = $".."
 var player: Player
+var drop_count = 2
+const COIN_03 = preload("res://Collectables/Currency/coin03.tscn")
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -11,6 +13,11 @@ func take_attack(attack: Attack):
 	slime.health -= attack.damage
 	if slime.health <= 0:
 		player.collect_xp(slime.xp)
+		for i in range(drop_count):
+			var coin: Coin03 = COIN_03.instantiate()
+			slime.get_parent().call_deferred("add_child", coin)
+			var spawn_offset = Vector2(randi_range(1, 15), randi_range(1, 15))
+			coin.global_position = slime.global_position + spawn_offset
 		slime.queue_free()
 
 func knockback(direction: Vector2, force: int) -> void:
