68cc9295b5e75b642a5b153fe634181e37692beb
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-11-17 01:28:27 -0500

    updated skeleton animations

--- matthews-journey/Enemies/Skeleton/skeleton.gd
+++ matthews-journey/Enemies/Skeleton/skeleton.gd
@@ -7,11 +7,17 @@ class_name Skeleton extends Enemy
 @export var speed: int = 25
 @export var xp: int = 2
 
+var direction = Vector2.ZERO
+var cardinal_direction : Vector2 = Vector2.DOWN
+const DIRECTIONS = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
 @onready var player: Player = $"../../Player"
+@onready var sprite: AnimatedSprite2D = $Sprite2D
+
 var knockback_velocity: Vector2 = Vector2.ZERO
 var knocked_back: bool = false
 var player_detected: bool = false
+var current_state : String = "idle"
 
 func _on_hit_box_body_entered(body: Node2D) -> void:
 	#print("skelly hitbox entered")
@@ -22,10 +28,15 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 		pass
 
 func _physics_process(delta: float) -> void:
-	if player_detected:
+	if player_detected and not current_state == "dying":
 		velocity = position.direction_to(player.position) * speed
 	else:
 		velocity = Vector2.ZERO
+		
+	direction = velocity.normalized()
+	update_direction()
+	update_animation()
+	
 	if knocked_back:
 		velocity = knockback_velocity
 	move_and_slide()
@@ -34,8 +45,57 @@ func _physics_process(delta: float) -> void:
 func _on_detection_zone_body_entered(body: Node2D) -> void:
 	if body is Player:
 		player_detected = true
+		current_state = "walk"
 
 
 func _on_detection_zone_body_exited(body: Node2D) -> void:
 	if body is Player:
 		player_detected = false
+		current_state = "idle"
+		
+	# Return's true if the cardinal direction has changed based on player direction, false if it has not.
+# The function will also update the cardinal direction if direction has changed enough.
+func update_direction() -> bool:
+	# if no player input then return false
+	if direction == Vector2.ZERO:
+		return false
+	
+	# Calculate current cardinal direction
+	var direction_id : int = int(round( 
+		# bias the direction towards the current cardinal direction to prevent weird player motion (example: moonwalking)
+		(direction + cardinal_direction * 0.1  
+		# Divide the angle of direction by 2*PI * 4. This converts direction to an integer 0-3 corresponding to an index in DIRECTIONS
+		).angle() / TAU * DIRECTIONS.size()))
+	# assign the new direction based on calculation above
+	var new_dir : Vector2 = DIRECTIONS[direction_id]
+	
+	# If player still facing the same direction as before return false
+	if new_dir == cardinal_direction:
+		return false
+	
+	# update the direction, flip sprite if needed, and return true (direction changed)
+	else:
+		cardinal_direction = new_dir
+		# flip the sprite if facing left
+		if cardinal_direction == Vector2.LEFT:
+			sprite.scale.x = -1
+		else:
+			sprite.scale.x = 1
+		return true
+
+# update the character animation based on state and current cardinal direction	
+func update_animation() -> void:
+	if current_state == "dying":
+		sprite.play("die")
+		#await sprite.animation_finished
+	else:
+		sprite.play(current_state + "_" + animation_direction())
+
+# Return a string representing the current facing cardinal direction
+func animation_direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
--- matthews-journey/Enemies/Skeleton/skeleton_hurt_box.gd
+++ matthews-journey/Enemies/Skeleton/skeleton_hurt_box.gd
@@ -5,6 +5,8 @@ var player: Player
 @onready var game: Node2D = $"."
 const COIN_03 = preload("res://Collectables/Currency/coin03.tscn")
 var drop_count = 4
+@onready var sprite: AnimatedSprite2D = $"../Sprite2D"
+@onready var hit_box: SkeletonHitBox = $"../HitBox"
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -14,16 +16,26 @@ func take_attack(attack: Attack):
 	knockback(attack.direction, attack.knockback)
 	skeleton.health -= attack.damage
 	if skeleton.health <= 0:
-		player.collect_xp(skeleton.xp)
-		for i in range(drop_count):
-			var coin: Coin03 = COIN_03.instantiate()
-			skeleton.get_parent().call_deferred("add_child", coin)
-			var spawn_offset = Vector2(randi_range(1, 15), randi_range(1, 15))
-			coin.global_position = skeleton.global_position + spawn_offset
-		skeleton.queue_free()
+		destroy()
 
 func knockback(direction: Vector2, force: int) -> void:
 	skeleton.knockback_velocity = (skeleton.global_position - direction) * force
 	skeleton.knocked_back = true
 	await get_tree().create_timer(0.1).timeout
 	skeleton.knocked_back = false
+
+func destroy():
+	skeleton.velocity = Vector2.ZERO
+	skeleton.current_state = "dying"
+	hit_box.monitoring = false
+	await sprite.animation_finished
+	player.collect_xp(skeleton.xp)
+	spawn_drops()
+	skeleton.queue_free()
+
+func spawn_drops():
+	for i in range(drop_count):
+		var coin: Coin03 = COIN_03.instantiate()
+		skeleton.get_parent().call_deferred("add_child", coin)
+		var spawn_offset = Vector2(randi_range(1, 15), randi_range(1, 15))
+		coin.global_position = skeleton.global_position + spawn_offset
