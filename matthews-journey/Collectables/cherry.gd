a58aa993785cc46d23c2fa96525b4b7ae659c18d
Virginia Richards <richards.76@wright.edu>
2024-11-19 11:09:44 -0500

    Corrected cherry

--- matthews-journey/Collectables/cherry.gd
+++ matthews-journey/Collectables/cherry.gd
@@ -7,13 +7,10 @@ extends Collectable #inheirits from collectable.gd
 
 func collect():
 	var player = get_tree().get_first_node_in_group("player")
-	
-	print("Before healing - Current Health:", player.currentHealth)
-	player.currentHealth = player.maxHealth
-	print("After healing - Current Health:", player.currentHealth, " Max Health:", player.maxHealth)
-	
-	player.healthChanged.emit(player.currentHealth)
-	animations.play("spin")
-	audio.play()
-	await animations.animation_finished
-	super.collect()
+	if player and player.currentHealth < player.maxHealth:
+		player.currentHealth = player.maxHealth
+		player.healthChanged.emit(player.currentHealth)
+		animations.play("spin")
+		audio.play()
+		await animations.animation_finished
+		super.collect()
