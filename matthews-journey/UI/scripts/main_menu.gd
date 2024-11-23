b4927becb9d1a590a1a5f5383a4eff9a3c183cd9
Robert Dallessandris <robert.dallessandris@gmail.com>
2024-11-23 14:18:54 -0500

    main menu now launches new final scene

--- matthews-journey/UI/scripts/main_menu.gd
+++ matthews-journey/UI/scripts/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_begin_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://finalGame.tscn")
 	
 func _on_controls_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/controls_menu.tscn")
