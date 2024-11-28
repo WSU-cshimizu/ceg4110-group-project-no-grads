e24332100f5f1506c69780f7f5e33ef0473008eb
RomanRhino <gatrios@woh.rr.com>
2024-11-27 19:25:31 -0500

    added a lot of stuff

--- matthews-journey/npcs/guard/detection_area.gd
+++ matthews-journey/npcs/guard/detection_area.gd
@@ -3,9 +3,12 @@ extends Area2D
 var chatting: bool = false
 var done_talking: bool = false
 @onready var chat_icon: Sprite2D = $ChatIcon
-@onready var player: Player = $Player
+@onready var player: Player = %Player
+@onready var metDolbz: bool = false
+
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
+	get_node("/")
 	if body is Player:
 		chatting = true
 		chat_icon.visible = true
@@ -17,8 +20,15 @@ func _on_body_exited(body: Node2D) -> void:
 		done_talking = false
 		
 func _input(event: InputEvent) -> void:
-	if chatting and not done_talking and event.is_action_pressed("dialogic_default_action"):
+	if chatting and not done_talking and not metDolbz and event.is_action_pressed("dialogic_default_action"):
 		done_talking = true
-		var dialog = Dialogic.start("tent_guard")
-		
-		
+		$"..".setHealth()
+		var dialog = Dialogic.start("Dolvalir")
+		metDolbz = true
+	elif chatting and not done_talking and metDolbz and event.is_action_pressed("dialogic_default_action"):
+		done_talking = true
+		$"..".setHealth()
+		var dialog = Dialogic.start("MetDolbz")
+	
+
+	
